db1e8f99af0aa6bfebcae5387abc5d68ac7d4852
vinicius <vinisibim@hotmail.com.br>
2018-12-14 16:48:03 -0200

    front end da calculadora

---
+++ calculadora-frontend/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  numeroUm: number;
+  numeroDois: number;
+  operacao: string;
+
+  response: any = {};
+
+  constructor(private http: HttpClient){
+
+  }
+
+
+    onClickExecutaroperacao(){
+      const calculo = {
+        numeroUm: this.numeroUm,
+        numeroDois: this.numeroDois,
+        operacao: this.operacao,
+      };
+
+      
+
+      this.http.post('http://localhost:9090/api/calculadora', calculo).subscribe(response => this.response = response)
+    }
+
+   
+}
---
+++ calculadora-frontend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { from } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
